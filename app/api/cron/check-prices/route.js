e2730a57a5bd1537efe17426272a6129429bf65b
Sidx005 <desaisiddharth1246@gmail.com>
2026-01-19 16:35:30 +0530

    5th commit

--- app/api/cron/check-prices/route.js
+++ app/api/cron/check-prices/route.js
@@ -39,7 +39,7 @@ export async function POST(req){
                     results.failed++;
                     continue;
                 }
-                const newPrice=parseFloat(res.currentPrice);
+                const newPrice=parseFloat(res.currentPrice)-4;
                 const oldPrice=parseFloat(product.current_price);
                 (await supabase).from("products").update({
                 current_price: newPrice,
